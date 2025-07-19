ca3f2fdc160c4f5f40e971ed8df582ac5db2b846
bigyanse <contact.bigyanse@gmail.com>
2025-07-19 11:16:59 +0545

    `add`: added `neogit` keymaps

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,6 +1,17 @@
 local plugins = {
   neogit = {
     "NeogitOrg/neogit",
+    keys = {
+      { "<leader>ngs", "<cmd>Neogit<cr>", desc = "Status", { noremap = true, silent = true } },
+      { "<leader>ngc", "<cmd>Neogit commit<cr>", desc = "Commit", { noremap = true, silent = true } },
+      { "<leader>ngp", "<cmd>Neogit push<cr>", desc = "Push", { noremap = true, silent = true } },
+      { "<leader>ngr", "<cmd>Neogit reset<cr>", desc = "Reset", { noremap = true, silent = true } },
+      { "<leader>ngb", "<cmd>Neogit blame<cr>", desc = "Blame", { noremap = true, silent = true } },
+      { "<leader>ngl", "<cmd>Neogit log<cr>", desc = "Log", { noremap = true, silent = true } },
+      { "<leader>ngD", "<cmd>Neogit diff<cr>", desc = "Diff", { noremap = true, silent = true } },
+      { "<leader>ngS", "<cmd>Neogit stage<cr>", desc = "Stage", { noremap = true, silent = true } },
+      { "<leader>ngU", "<cmd>Neogit unstage<cr>", desc = "Unstage", { noremap = true, silent = true } },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
       "sindrets/diffview.nvim", -- optional - Diff integration
