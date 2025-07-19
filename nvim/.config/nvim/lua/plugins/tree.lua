0c63b091ef22fb117c6c091ed8281ceac3a6caaf
bigyanse <contact.bigyanse@gmail.com>
2025-07-19 11:01:00 +0545

    `replace`: replaced `nvim-tree` with `neo-tree`

---
+++ nvim/.config/nvim/lua/plugins/tree.lua
@@ -0,0 +1,51 @@
+local plugins = {
+  neotree = {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- Optional image support for file preview: See `# Preview Mode` for more information.
+      -- {"3rd/image.nvim", opts = {}},
+      -- OR use snacks.nvim's image module:
+      -- "folke/snacks.nvim",
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+      -- add options here
+    },
+    keys = {
+      { "<leader>ntt", ":NvimTreeToggle<CR>", desc = "NvimTreeToggle" },
+    },
+  },
+  nvimtree = {
+    "nvim-tree/nvim-tree.lua",
+    keys = {
+      { "<leader>ntt", ":NvimTreeToggle<CR>", desc = "NvimTreeToggle" },
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup({
+        sort = {
+          sorter = "case_sensitive",
+        },
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = true,
+        },
+      })
+    end,
+  },
+}
+
+return plugins["neotree"]
